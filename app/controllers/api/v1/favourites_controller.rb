d998ef585c5c5f82d7e41ef82fcd5624e01b7d41
alexoid <alex_zam@hotmail.es>
2021-02-18 15:28:19 -0500

    add routes an favourites model

---
+++ app/controllers/api/v1/favourites_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::FavouritesController < ApplicationController
+    def index
+      @user_favourites = User.find(params[:user_id]).favourites
+      render json: @user_favourites
+    end
+  
+    def create
+      motocycle = Motocycle.find_by name: params[:name], model: params[:model], image_url: params[:image_url]
+  
+      if motocycle
+        user_favourites = User.find(params[:user_id]).motocycles
+        user_favourites << motocycle
+        render json: user_favourites
+      else
+        render json: { status: 'error', message: "can't find a Motocycle with these props" }
+      end
+    end
+  
+    def destroy
+      motocycle = @user_favourites.find_by bicycle_id: params[:id]
+      if motocycle
+        @user_favourites = User.find(params[:user_id]).favourites
+        @user_favourites.delete(motocycle)
+        render json: @user_favourites
+      else
+        render json: { status: 'error', message: "can't find a Motocycle with the id #{params[:id]}" }
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1, :defaults => {:format => :json} do
       resources :motocycles ,  only: [:index, :show, :create, :update, :destroy]
-      resources :users ,  only: [:index, :show, :create, :update, :destroy] do 
+      resources :users ,  only: [:index, :show, :create, :update, :destroy] do
+        resources :favourites, only: [:index, :create, :destroy]
       
       end
     end
